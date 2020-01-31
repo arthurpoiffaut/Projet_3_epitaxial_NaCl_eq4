3bb05be7d12f0a882fd804aa12341696aa688210
Unknown <didier.blachlafleche@gmail.com>
2020-01-31 13:07:56 -0500

    master  initialisation

--- mini_teste/1/minitest1_main.m
+++ mini_teste/1/minitest1_main.m
@@ -1,20 +1,21 @@
 %main
 
 %initialisation
-k = 1
-g = 1
-m = 1
-dt= 0.01
-numberOfSteps = 100
-y0 = 1
-v0k = 1
+alpha =1;
+k = 1;
+g = 1;
+m = 1;
+dt= 0.01;
+numberOfSteps = 10;
+y0 = 1;
+v0 = 1;
 
 
 
 
 y = ones(1,numberOfSteps)
-y[1] =1
-y[2] = (1-(k*dt^2)/2m)*y0 + dt*(1-(alpha*dt/2*m))*v0 - (g*dt^2)/2 ;
+y(1) =y0;
+y(2) = (1-(k*dt^2)/2*m)*y0 + dt*(1-(alpha*dt/2*m))*v0 - (g*dt^2)/2 ;
 
-for i = (1,numberOfSteps)
-y[i] = calculetY( dt,yi,yi2,k,m,alpha,g)
+% for i = (1,numberOfSteps)
+%     y[i] = calculetY( dt,yi,yi2,k,m,alpha,g)
