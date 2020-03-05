ccd0a0f1c2f4626eca80116ea7f77735ae7ac403
arthur <tuthurus@gmail.com>
2020-03-05 16:45:52 -0500

    de l'ostie de marde

--- mini_test/4/Equation_nonlineaire_independante_du_temps.m
+++ mini_test/4/Equation_nonlineaire_independante_du_temps.m
@@ -1,4 +1,6 @@
+%% init
 clear
+
 % Équation différentielle: d^2 u/dx^2=g(x) sur x=(a,b)
 % Conditions aux limites générales:
 % x=a: c1*du/dx+c2*u+c3=-si*T^4
@@ -10,12 +12,17 @@ L=0.3; %m ; Épaisseur du mur
 k=0.85;%W/(m*K); La conductivité thermique de la brique
 si=5.67e-8; %constante de Stefan-Boltzmann
 
+%TL=841.90; NONONONONON
 To=293; %oK
 % Condition radiative à x=0 (face externe du mur): -k*dT/dx=-si*(T^4-To^4)
 % !!! Condition radiative est implementée SEULEMENT sur la face externe du mur !!!
-c1=-k; c2=0; c3=-si*To^4;
+c1=-k;
+c2=0;
+c3=-si*To^4;
 % Condition de Neumann à x=L (face interne du mur): dT/dx=0
-d1=1; d2=0; d3=0;
+d1=-k;
+d2=0;
+d3=-si*To^4;
 
 %(N+1) nœuds dans la maille
 % Nmax=10000 pour 1G de mémoire
@@ -33,32 +40,60 @@ T=To*ones(size(x)); % Approximation initiale
 
 % Sourse volumique de chaleur q[W/m^3] d'épaisseur dL
 % La source est intégrée dans la partie intérieure du mur
-dL=0.1;
-q=2000;% W/m^3;
+dL=0.05;
+q=10^6;% W/m^3;
 S=q*exp(-((x-L)/dL).^2);
 
 figure(1);plot(x,T,'r');
 xlabel('x [m]');ylabel('T [K]');hold
 % boucle de convergence
-ci=0;Err=[];tol=1e-12;flag=1;
+ci=0;
+Err=[];
+tol=1e-12;
+flag=1;
+
+%% resolution
 while (flag==1)
     ci=ci+1;
     
     M=diag(-2*ones(1,N+1),0)+diag(ones(1,N),-1)+diag(ones(1,N),1);
-    M(1,1)=2*c2*dx-3*c1;M(1,2)=4*c1;M(1,3)=-c1;
-    M(N+1,N+1)=3*d1+2*d2*dx;M(N+1,N)=-4*d1;M(N+1,N-1)=d1;
+    
+    % condition x=0
+    M(1,1)=-3*c1;
+    M(1,2)=4*c1;
+    M(1,3)=-c1;
+    % -k*dT/dx=-si*(T^4-To^4)
+    %c1=-k;
+    %c2=0;
+    %c3=-si*To^4;
+    
+    %d1=-k;
+    %d2=0;
+    %d3=si*TL^4;
+    % condition x=L
+    M(N+1,N+1)=3*d1;
+    M(N+1,N)=-4*d1;
+    M(N+1,N-1)=d1;
     
     % !!! Condition radiative est implementée SEULEMENT sur la face externe du mur !!!
-    b=S/k*dx^2; b(1)=T(1)^4*2*dx*si+2*c3*dx; b(N+1)=2*d3*dx;
+    b=(S./k).*dx^2; 
+    %condition x=0
+    b(1)=-2*dx^2*(To-T(1))+2*dx*(T(1)^4*si+c3);%%-2*dx^2*(To-T(1))+2*dx*(T(1)^4*si+c3); 
+    %conditon x=L
+    b(N+1)=-2*dx^2*(To-T(N))+2*dx*(T(N)^4*si-d3);%-2*dx^2*(To-T(N+1))+2*dx*(T(N+1)^4*si+d3);%=2*dx*(T(N+1)^4*si+d3);%-2*dx^2*(To-T(N+1))+%2*dx*(T(N+1)^4*si+d3);%-2*dx*(T(N+1)^4*si+d3);%2*dx^2*(To-T(N+1))-2*dx*(T(N+1)^4*si+d3);%2*d3*dx;
     
     F=M*T+b;
     Err=[Err sum(abs(F))/(N+1)];
     display(['Étape=', num2str(ci), '   ;   Err=' num2str(Err(end))])
-    if (Err(end)<tol) break; end
+    if (Err(end)<tol) 
+        break ; % metre flag ==0 ? jai la fleme
+    end
     pause
+    % % pas sur de l'utiliter
     
     % !!! Condition radiative est implementée SEULEMENT sur la face externe du mur !!!
-    J=M;J(1,1)=M(1,1)+T(1)^3*8*dx*si;
+    J=M;
+    J(1,1)=M(1,1)+T(1)^3*8*dx*si;
     
     dT=-J\F;
     T=T+dT;
