eb2a813294e8ddfe417fb668d65a04a0d4f91cdb
Alexis <alexis_seguin01@hotmail.com>
2020-03-24 16:21:25 -0400

    update, fct hex

---
+++ CODE/Functions/FonctionHex.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 15:38:56 2020
+
+@author: Alexis
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+dimx=100
+dimy=100
+
+mattest = np.zeros((dimx,dimy))
+mattest[45:55,45:55]=2
+mattest[40:45,40:60]=1
+mattest[55:60,40:60]=1
+mattest[45:55,40:45]=1
+mattest[45:55,55:60]=1
+
+
+#mattest[40:45,40:55]=1
+#mattest[45:55,40:60]=1
+#mattest[45:55,40:45]=1
+#mattest[55:60,45:60]=1
+
+plt.imshow(mattest,interpolation='spline16',cmap='viridis')
+
+
+
+
+def hexplot(matflocon):#prend la matrice 2D contenant l'image du flocon et ''l'hexagonifie'' en scatter
+    hexagonal = np.empty((2,dimx,dimy)) #array contenant les points hex
+    a1 = np.array([1., -1./np.sqrt(3.)]) #vecteur de la base hexagonale
+    a2 = np.array([1., 1./np.sqrt(3.)])
+    for i in range(dimx):
+        for j in range(dimy):
+            hexagonal[:,i,j] = int(i)*a1 + int(j)*a2
+    index_flocon = np.where(matflocon >= 1)  #0 si on veut tous les points de la matrice.
+    flocon = hexagonal[:,index_flocon[0],index_flocon[1]]
+    fig = plt.figure()
+    plt.scatter(flocon[0,:], flocon[1,:], c =matflocon[index_flocon[0],\
+                 index_flocon[1]]-1., s = 10., \
+                    cmap = 'cubehelix', marker = 'H')
+    fig.savefig('testfloconhex_'+'mx'+str(dimx)+'my'+str(dimy)+'.png') #Enregistrement de la figure au même endroit où le code .py est situé
+    plt.axis("off")
+    plt.show()
+
+
+hexplot(mattest)
